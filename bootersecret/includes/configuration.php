b4f8a68583d78eeb1c7abcfdc643262be718bf90
kaleid1337 <39824802+kaleid1337@users.noreply.github.com>
2020-10-10 14:47:26 +0800

    Update configuration.php

--- bootersecret/includes/configuration.php
+++ bootersecret/includes/configuration.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'epiz_26933006_hexed');
-define('DB_USERNAME', 'epiz_26933006');
-define('DB_PASSWORD', 'WcWaODXqnaZ');
+define('DB_HOST', 'sql308.epizy.com');
+define('DB_NAME', 'epiz_26933323_hexed');
+define('DB_USERNAME', 'epiz_26933323');
+define('DB_PASSWORD', '9Xianwf4Y5');
 define('ERROR_MESSAGE', 'Could not connect to database.');
 //Make sure to give DB Full access.
 
